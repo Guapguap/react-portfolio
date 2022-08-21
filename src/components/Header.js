78ae3be1e69fde8db8fcfe60b7d7bf3d5a8559bc
alberto londono <albertolondono.londono5@gmail.com>
2022-08-21 15:50:20 -0500

    Create new file to hold resume pdf. Create new Resume component. Modify Header component with tabs. Modify portfolio to import resume component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,23 @@ import React from 'react';
 
 export default function Header () {
     return (
-        <h1>Londono</h1>
+        <nav className='navbar'>
+            <h1>Londono</h1>
+            <ul> 
+                <li>
+                    <a href="#about-me">About Me</a>
+                </li>
+                <li>
+                    <a href="#resume">Resume</a>
+                </li>
+                <li>
+                    <a href="#projects">Projects</a>
+                </li>
+                <li>
+                    <a href="#contact-me">Contact Me</a>
+                </li>
+            </ul>
+
+        </nav>
     );
 }
---
+++ src/components/Resume.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Resume() {
+  return (
+    <div>
+      <li>
+        <a href="../../public/assets/Londono_Resume.pdf" download>
+          Download Resume
+        </a>
+      </li>
+    </div>
+  );
+}
