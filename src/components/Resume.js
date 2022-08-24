0d205273023de5b801c6dab7b60ff746c4770e52
alberto londono <albertolondono.londono5@gmail.com>
2022-08-24 16:35:03 -0500

    Modify title in index.html. Modify Resume.js with contact info. summary, and skills and downloadable resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,23 +2,33 @@ import React from "react";
 
 export default function Resume() {
   return (
-    <div>
-      <li>
-        <a href="../../assets/Londono_Resume.pdf" download>
-          Download Resume
-        </a>
-      </li>
-      <p>I have experience working with:</p>
-      <ul className="about-me-text">
-        <li>JavaScript ES5 + ES6</li>
-        <li>HTML5 + CSS3</li>
-        <li>React.js</li>
-        <li>Node.js</li>
-        <li>Express.js</li>
-        <li>MongoDB + Mongoose</li>
-        <li>MySQL + Sequelize</li>
-        <li>Third Party APIs</li>
-      </ul>
+<div className="container">
+<div className="row">
+    <div className="col-md-12">
+        <div classname="resume">
+            <div className="resume-header">
+                <h3>Alberto Londono</h3>
+                <p>312-972-2140 • Evanston, IL 60201</p>
+
+<p><a href="mailto: londono.alberto110@gmail.com"> Email: londono.alberto110@gmail.com</a></p>
+
+<p><a href="https://github.com/Guapguap">GitHub: https://github.com/Guapguap</a></p>
+
+<h5><a href="../../assets/Londono_Resume.pdf" download> Download Resume
+        </a></h5>
+            </div>
+            
+            <h5>SUMMARY</h5>
+            <p>Full stack web developer/software engineer and dynamic problem solver. Aspiring Game Developer with high enthusiasm and interest in the gaming industry. Exceptional interpersonal skills with the ability to collaborate and contribute in a team setting. Strong time management and organizational abilities. </p>
+
+            <br />
+            <h5>TECHNICAL SKILLSs</h5>
+            <p>Coding: JavaScript ES5/6 • HTML5 • CSS • React.js • Node.js • Express.js • MySQL • MongoDB • Third Party APIs
+<br /></p>
+
+</div>
     </div>
+</div>
+</div>
   );
 }
