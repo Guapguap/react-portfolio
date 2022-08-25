c8d7246dcdf20fbbd644e0624c5d97734247585f
alberto londono <albertolondono.londono5@gmail.com>
2022-08-24 22:30:47 -0500

    Modify contact with input fields and labels.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Contact () {
+
+
     return (
-        <p>I am the contact</p>
+        <section className="container">
+        <h2 data-testid='h1tag' className="top-title">Contact Form</h2>
+        <hr></hr>
+        <form class="justify-content-center" id="contact-form">
+            <div class="mt-5" >
+                <label htmlFor="name">Name:</label>
+                <input class="form-control" type="text" name="name"  defaultValue={name} onBlur={handleChange}/>
+            </div>
+            <div class="mt-5" >
+                <label htmlFor="email">Email Address:</label>
+                <input class="form-control" type="email"  name="email" defaultValue={email} onBlur={handleChange} />
+            </div>
+            <div class="mt-5" >
+                <label htmlFor="message">Message:</label>
+                <textarea class="form-control" name="message" defaultValue={message} onBlur={handleChange} rows="7" />
+            </div> 
+            {errorMessage && (
+            <div>
+                <p className="error-text">{errorMessage}</p>
+            </div>
+            )}
+
+            <div class="mt-5 mb-5" >
+            <button data-testid='button' class="btn btn-outline-dark " type="submit" onSubmit={handleSubmit}>Submit</button>
+            </div>
+        </form>
+    </section>
     )
 }
