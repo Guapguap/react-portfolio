4059a91ef14d44fd33b71b1e1613d6a322a12516
alberto londono <albertolondono.londono5@gmail.com>
2022-08-24 21:14:11 -0500

    Modify styling. Modify resume.js content. summary and skills included with links to contact info.

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,33 +2,54 @@ import React from "react";
 
 export default function Resume() {
   return (
-<div className="container">
-<div className="row">
-    <div className="col-md-12">
-        <div classname="resume">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <div classname="resume">
             <div className="resume-header">
-                <h3>Alberto Londono</h3>
-                <p>312-972-2140 • Evanston, IL 60201</p>
+              <h3>Alberto Londono</h3>
+              <p>312-972-2140 • Evanston, IL 60201</p>
 
-<p><a href="mailto: londono.alberto110@gmail.com"> Email: londono.alberto110@gmail.com</a></p>
+              <p>
+                <a href="mailto: londono.alberto110@gmail.com">
+                  {" "}
+                  Email: londono.alberto110@gmail.com
+                </a>
+              </p>
 
-<p><a href="https://github.com/Guapguap">GitHub: https://github.com/Guapguap</a></p>
+              <p>
+                <a href="https://github.com/Guapguap">
+                  GitHub: https://github.com/Guapguap
+                </a>
+              </p>
 
-<h5><a href="../../assets/Londono_Resume.pdf" download> Download Resume
-        </a></h5>
+              <h5>
+                <a href="../../assets/Londono_Resume.pdf" download>
+                  {" "}
+                  Download Resume
+                </a>
+              </h5>
             </div>
-            
+
             <h5>SUMMARY</h5>
-            <p>Full stack web developer/software engineer and dynamic problem solver. Aspiring Game Developer with high enthusiasm and interest in the gaming industry. Exceptional interpersonal skills with the ability to collaborate and contribute in a team setting. Strong time management and organizational abilities. </p>
+            <p>
+              Full stack web developer/software engineer and dynamic problem
+              solver. Aspiring Game Developer with high enthusiasm and interest
+              in the gaming industry. Exceptional interpersonal skills with the
+              ability to collaborate and contribute in a team setting. Strong
+              time management and organizational abilities.{" "}
+            </p>
 
             <br />
             <h5>TECHNICAL SKILLSs</h5>
-            <p>Coding: JavaScript ES5/6 • HTML5 • CSS • React.js • Node.js • Express.js • MySQL • MongoDB • Third Party APIs
-<br /></p>
-
-</div>
+            <p>
+              JavaScript ES5/6 • Python • HTML5 • CSS • React.js • Node.js •
+              Express.js • MySQL • MongoDB • Third Party APIs
+              <br />
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
-</div>
   );
 }
