53ba2efa65d5878ed7559969275fda122220cb8b
alberto londono <albertolondono.londono5@gmail.com>
2022-08-26 20:20:28 -0500

    Add new images for projects. Modify styling for Portfolio page.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,34 +3,32 @@ import React from "react";
 import project from "../project.json";
 
 function Wrapper(props) {
-  return <div className="wrapper">{props.children}</div>;
+  return <div className="d-sm-flex flex-wrap ">{props.children}</div>;
 }
 
 function ProjectCard(props) {
   return (
-    <div className="card">
-      <div className="img-container">
-        <img alt={props.name} src={props.image} />
-      </div>
-      <div className="content">
-        <p className="card-title">{props.name}</p>
-        <p className="proj-icons-container">
-          <a href={props.github}>
-            <img
-              className="project-icon"
-              src="https://img.icons8.com/fluent/48/000000/github.png"
-              alt="GitHub Repo"
-            />
-          </a>
-          <a href={props.link}>
-            <img
-              className="project-icon"
-              src="https://img.icons8.com/fluent/48/000000/domain.png"
-              alt="Live Application"
-            />
-          </a>
-        </p>
-        <p className="tech">({props.tech})</p>
+    <div className="container mb-5">
+      <div className="row">
+        <div className="col-sm">
+          <h3 className="project-text">{props.name}</h3>
+          <img className="project-img mx-auto img-thumbnail" alt={props.name} src={props.image} />
+          <div className="content">
+            <p className="project-text links">
+            <span className="project-icon">
+              <a href={props.github} target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-github-square">
+
+                </i>
+              </a>
+              </span>
+              <a href={props.link} target="_blank" rel="noopener noreferrer">
+              <i class="fas fa-link"></i>
+              </a>
+            </p>
+            <p className="tech project-text">({props.tech})</p>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -38,24 +36,17 @@ function ProjectCard(props) {
 
 export default function Projects() {
   return (
-    <section className="container">
-      <div className="project">
-        <h2 className="top-title">Bootcamp Portfolio</h2>
-        <hr></hr>
-      </div>
-
-      <Wrapper id="card-data">
-        {project.map((project) => (
-          <ProjectCard
-            key={project.id}
-            image={project.image}
-            name={project.name}
-            github={project.github}
-            link={project.link}
-            tech={project.tech}
-          />
-        ))}
-      </Wrapper>
-    </section>
+    <Wrapper>
+      {project.map((project) => (
+        <ProjectCard
+          key={project.id}
+          image={project.image}
+          name={project.name}
+          github={project.github}
+          link={project.link}
+          tech={project.tech}
+        />
+      ))}
+    </Wrapper>
   );
 }
